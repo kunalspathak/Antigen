6fa9b588a52fa668b11a92b2829f60851a289878
Kunal Pathak <Kunal.Pathak@microsoft.com>
2023-10-04 22:24:40 -0700

    version 6

--- test.py
+++ test.py
@@ -33,6 +33,7 @@ if len(rows) > 1:
     
     # Extract the data from each cell with word replacements
     extracted_data = []
+    non_numeric_output = []
     for cell in cells:
         # Check if the cell spans multiple columns
         if 'colspan' in cell.attrs:
@@ -41,15 +42,28 @@ if len(rows) > 1:
             # Replace words using the regex pattern for the entire cell content
             cell_text = pattern.sub(lambda x: replacement_mapping.get(x.group(0), x.group(0)), cell_text)
             extracted_data.extend([cell_text] * colspan)
+            # Replace non-numeric output with 0 for the second output
+            non_numeric_output.extend(['0'] * colspan)
         else:
             cell_text = cell.get_text().strip()
             # Replace words using the regex pattern
             cell_text = pattern.sub(lambda x: replacement_mapping.get(x.group(0), x.group(0)), cell_text)
             extracted_data.append(cell_text)
+            # Replace non-numeric output with 0 for the second output
+            non_numeric_output.append('0')
 
     # Print the extracted data with word replacements
+    print("Extracted Data:")
     print(extracted_data)
+
+    # Print the second output with non-numeric values replaced by 0
+    print("\nSecond Output (Non-numeric values replaced by 0):")
+    print(non_numeric_output)
 else:
     print("Table does not have enough rows.")
     
+# Extracted Data:
 # ['sf', '1', '0', '1', '1', '0', '1', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0', 'n', 'n', 'n', 'n', 'n', 'd', 'd', 'd', 'd', 'd']
+
+# Second Output (Non-numeric values replaced by 0):
+# ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
